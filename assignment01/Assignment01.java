a1a409635bb2d730f38f82a57e3fee885dad45bc
gaurav <gauravwasekar7@gmail.com>
2022-07-18 21:55:11 +0530

    assignment - 1 added

--- assignment01/Assignment01.java
+++ assignment01/Assignment01.java
@@ -80,7 +80,22 @@ public class Assignment01 {
 //        }
 
 //        8. To find out whether the given String is Palindrome or not.
+                System.out.println("enter the String:- ");
+                String str = sc.nextLine();
+                String reverseStr = "";
 
+        int strLength = str.length();
+
+        for (int i = (strLength - 1); i >=0; --i) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+            System.out.println(str + " is a Palindrome String.");
+        }
+        else {
+            System.out.println(str + " is not a Palindrome String.");
+        }
 
 //        9. To find Armstrong Number between two given number.
     }
