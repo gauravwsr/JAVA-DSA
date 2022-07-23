35d441e98e1b1c3dac6decccf93bea9c64a5316e
gaurav <gauravwasekar7@gmail.com>
2022-07-23 14:53:02 +0530

    Simple_Calculator file added

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String [] args){
-        System.out.println("Hello my love");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your salary amount:- ");
+        int Salary = sc.nextInt();
+
+        if (Salary > 10000){
+            int bouns = Salary + 1000;
+            System.out.println("boohoo, you have an increment of 1000 \n         Now Your Total Salary of this months is "+bouns);
+        }else {
+            System.out.println("Sorry buddy no bonus this time for you \n          your Salary is as usual "+Salary);
+        }
     }
 }
