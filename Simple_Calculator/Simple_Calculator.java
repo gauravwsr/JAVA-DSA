85284abdad828688b641dff0a543f5fa8a565f3d
gaurav <gauravwasekar7@gmail.com>
2022-07-25 12:59:36 +0530

    Calculator file added

--- Simple_Calculator/Simple_Calculator.java
+++ Simple_Calculator/Simple_Calculator.java
@@ -4,18 +4,46 @@ import java.util.Scanner;
 public class Simple_Calculator {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
 
-        int max = a;
-        if (b >max){
-            max = b;
-        }
-        if (c >max){
-            max = c;
-        }
-        System.out.println(max);
+        int ans = 0;
 
+        while (true){
+            System.out.println("Enter the Operator:- ");
+            char op = sc.next().trim().charAt(0);
+            System.out.println();
+
+            if (op == '+'|| op == '-'|| op == '*' || op =='/' || op =='%'){
+                System.out.println("Enter the Value of A :- ");
+                int num1 = sc.nextInt();
+                System.out.println("Enter the Value of B :- ");
+                int num2 = sc.nextInt();
+                if (op=='+'){
+                    ans = num1+num2;
+                }
+                if (op=='-'){
+                    ans = num1-num2;
+                }
+                if (op=='*'){
+                    ans = num1*num2;
+                }
+                if (op=='/'){
+                    if (num1!=0||num2!=0){
+                        ans = num1/num2;
+                    }else {
+                        System.out.println("0 can't be divide to any number!");
+                    }
+                }
+                if (op=='%'){
+                    ans = num1%num2;
+                }
+                System.out.println("\n Your Ans = "+ans);
+                System.out.println();
+                System.out.println("Enter 'X' or 'x' to exit!");
+            } else if (op == 'X'||op =='x') {
+                break;
+            }else {
+                System.out.println("Something happened wrong enter again !!"+"\n");
+            }
+        }
         }
 }
